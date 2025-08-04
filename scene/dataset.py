5c2b3215a0317dd05f554dd4e36b3c9dda82fd09
Ray <carminasonata@gmail.com>
2025-08-04 13:06:33 +0200

    add support for hypernerf

--- scene/dataset.py
+++ scene/dataset.py
@@ -72,10 +72,6 @@ class FourDGSdataset(Dataset):
                 return Camera(colmap_id=index, R=R, T=T, FoVx=FovX, FoVy=FovY, image=image, gt_alpha_mask=None,
                                       image_name=f"{index}", uid=index, data_device=torch.device("cuda"), time=time,
                                       mask=mask, SD_feature=1)
-            if caminfo.SD_feature is not None:
-                return Camera(colmap_id=index,R=R,T=T,FoVx=FovX,FoVy=FovY,image=image,gt_alpha_mask=None,
-                              image_name=caminfo.image_name,uid=index,data_device=torch.device("cuda"),time=time,
-                              mask=mask,SD_feature=caminfo.SD_feature)
             return Camera(colmap_id=index,R=R,T=T,FoVx=FovX,FoVy=FovY,image=image,gt_alpha_mask=None,
                               image_name=caminfo.image_name,uid=index,data_device=torch.device("cuda"),time=time,
                               mask=mask)
